0274831e5a66c758ff5b4c072cf81488ddc35ed2
AshifAli007 <theashifkhan@gmail.com>
2021-11-06 14:25:37 +0530

    changes updated

--- src/components/choices/choices.js
+++ src/components/choices/choices.js
@@ -5,7 +5,7 @@ import styles from './choices.module.css';
 const Choices = () =>{
     const timeToLive = () =>{
         let today = new Date();
-        let endTime = new Date(2021, 10, 6, 10, 0, 0, 0);
+        let endTime = new Date(2021, 10, 8, 10, 0, 0, 0);
         console.log('endTime', endTime);
         let diffInMilliSecs = Date.parse(endTime) - today.getTime();
 
